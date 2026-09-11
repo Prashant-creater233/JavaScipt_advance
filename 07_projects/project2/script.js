c83a282b2017785a1e1bf0fc4f6c78c0edd1f8bb
Prashant-creater233 <prashantkanwal8@gmail.com>
2026-09-11 09:32:12 +0530

    Add color scheme switcher with buttons to change background color

---
+++ 07_projects/project2/script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('form')
+
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+
+    if(height === '' || height < 0 || isNaN(height)){
+        results.innerHTML =  `Please give a valid height ${height}`;
+    } else if(weight === '' || weight < 0 || isNaN(weight)){
+        results.innerHTML =  `Please give a valid weight ${weight}`;
+    } else {
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
+        // show the result
+        results.innerHTML = `<span>${bmi}</span>`
+        
+        // if (results < 18.6) {
+        //     results.innerHTML = 'Under Weight = Less than 18.6'
+        // } else if(results > 18.6 && results < 24.9) {
+        //     results.innerHTML = 'Normal Range = 18.6 and 24.9'
+        // } else {
+        //     results.innerHTML = 'Overweight'
+        // }
+    }
+})
